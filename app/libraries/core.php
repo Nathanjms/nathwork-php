092279897e1ab902f28a40b73c8d85226d1b5921
Nathan James <nathanjms@outlook.com>
2021-08-28 15:34:20 +0100

    Loading the Controller from the URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -13,11 +13,29 @@ class Core
 
     function __construct()
     {
-        $this->getUrl();
+        // print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        // Look in controllers for first value
+        if($url && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+
+        // Require controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        
+        // Instantiate controller class
+        $this->currentController = new $this->currentController;
     }
 
     public function getUrl()
     {
-        echo $_GET['url'];
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
