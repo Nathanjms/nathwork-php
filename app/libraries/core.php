c03ecc3151b92c879e92743f7d56a084936d5f94
Nathan James <nathanjms@outlook.com>
2021-08-28 15:51:49 +0100

    Mapping Methods & Parameters

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -17,7 +17,7 @@ class Core
         $url = $this->getUrl();
 
         // Look in controllers for first value
-        if($url && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+        if(isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
             $this->currentController = ucwords($url[0]);
             unset($url[0]);
         }
@@ -27,6 +27,20 @@ class Core
         
         // Instantiate controller class
         $this->currentController = new $this->currentController;
+
+        // Check for second part of url & check to see if method exists in controller
+        if (isset($url[1]) && method_exists($this->currentController, $url[1])) {
+            $this->currentMethod = $url[1];
+            unset($url[1]);
+        }
+
+        // Get params
+
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+
     }
 
     public function getUrl()
