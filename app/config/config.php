d87bb6fb519ea93a9474392f397004ec96ca8a06
Nathan James <nathanjms@outlook.com>
2021-08-28 22:04:12 +0100

    Cleanup

--- app/config/config.php
+++ app/config/config.php
@@ -9,4 +9,4 @@ define('APPROUTE', dirname(dirname(__FILE__)));
 // URL route
 define('URLROUTE', 'http://localhost:8080/course');
 // Site Name
-define('SITENAME', 'NJMVC');
+define('SITENAME', 'NathWork PHP');
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,2 @@
 <h1><?= $data['title']; ?></h1>
-<h2>Posts:</h2>
-<?php foreach ($data['posts'] as $post) : ?>
-    <h3>ID: <?= $post->id; ?></h3>
-    <h3>Title<?= $post->title; ?></h3>
-    <br>
-<?php endforeach; ?>
+<p>This is the NathWork PHP Framework</p>
